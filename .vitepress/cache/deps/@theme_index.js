d8bafcd82f5d1a87dfe22ab31a4752938a2f8dff
送你十七束花 <108746194+030liuxinghao@users.noreply.github.com>
2023-12-21 20:50:12 +0800

    chore: add markdwown & commitlint

--- .vitepress/cache/deps/@theme_index.js
+++ .vitepress/cache/deps/@theme_index.js
@@ -10,31 +10,31 @@ import {
   watchEffect
 } from "./chunk-AQA7MYE3.js";
 
-// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/index.js
-import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/index.js
+import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
-// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default3 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default4 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default5 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default6 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default7 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default8 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default9 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default10 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default11 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default12 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/without-fonts.js
+import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default3 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default4 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default5 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default6 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default7 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default8 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default9 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default10 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default11 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default12 } from "E:/LeoStar の 大前端之旅/project/leostar-spec/node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/.pnpm/@vueuse+shared@10.7.0_vue@3.3.13/node_modules/@vueuse/shared/index.mjs
 function tryOnScopeDispose(fn) {
@@ -184,22 +184,22 @@ var _TransitionPresets = {
 };
 var TransitionPresets = Object.assign({}, { linear: identity }, _TransitionPresets);
 
-// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/shared.js
+// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/shared.js
 var inBrowser = typeof document !== "undefined";
 
-// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/support/utils.js
+// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/support/utils.js
 import { withBase } from "vitepress";
 
-// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/composables/data.js
+// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/composables/data.js
 import { useData as useData$ } from "vitepress";
 var useData = useData$;
 
-// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/support/utils.js
+// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/support/utils.js
 function ensureStartingSlash(path) {
   return /^\//.test(path) ? path : `/${path}`;
 }
 
-// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/support/sidebar.js
+// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/support/sidebar.js
 function getSidebar(_sidebar, path) {
   if (Array.isArray(_sidebar))
     return addBase(_sidebar);
@@ -242,7 +242,7 @@ function addBase(items, _base) {
   });
 }
 
-// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/composables/sidebar.js
+// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/composables/sidebar.js
 function useSidebar() {
   const { frontmatter, page, theme: theme2 } = useData();
   const is960 = useMediaQuery("(min-width: 960px)");
@@ -305,7 +305,7 @@ if (inBrowser) {
   });
 }
 
-// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/without-fonts.js
+// node_modules/.pnpm/vitepress@1.0.0-rc.32_@algolia+client-search@4.22.0_@types+node@20.10.5_search-insights@2.13.0_typescript@5.3.3/node_modules/vitepress/dist/client/theme-default/without-fonts.js
 var theme = {
   Layout,
   enhanceApp: ({ app }) => {
---
+++ packages/markdownlint-config/README.md
@@ -0,0 +1,25 @@
+## Markdownlint Config
+
+This is the shared markdownlint configuration for [leostar](https://github.com/ileostar) scoped projects.
+
+## Getting Started
+
+```bash
+# NPM
+npm install @ileostar/markdownlint-config markdownlint --save-dev
+# Yarn
+yarn add @zthun/markdownlint-config markdownlint --dev
+# Pnpm
+pnpm add @zthun/markdownlint-config markdownlint --dev
+```
+
+## Usage
+
+In the markdownlint config file, add the following
+
+```json
+{
+  "extends": ["@ileostar/markdownlint-config"]
+}
+```
+
